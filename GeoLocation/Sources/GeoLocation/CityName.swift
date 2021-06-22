c4cf47744009a32b7939030c6421db6388d4ea6c
Julia Bulantseva <bulantsevajo@gmail.com>
2021-06-22 23:26:35 +0700

    Updated main page code.
    1. Added finding the city
    2. Remove stop tracking, replaced it to update tracking
    3. Replaced buttons with city to dropdown list with cities

---
+++ GeoLocation/Sources/GeoLocation/CityName.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Danil Kristalev on 13.06.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CityName: Decodable {
+  var name: String
+}
