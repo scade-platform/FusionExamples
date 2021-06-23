a918a3009170f34cc22a41e3e527f6f0ec0e818f
Julia Bulantseva <bulantsevajo@gmail.com>
2021-06-23 16:10:45 +0700

    Removed copyright.

--- GeoLocation/Sources/GeoLocation/CityName.swift
+++ GeoLocation/Sources/GeoLocation/CityName.swift
@@ -1,11 +1,3 @@
-//
-//  WeatherData.swift
-//  Clima
-//
-//  Created by Danil Kristalev on 13.06.2021.
-//  Copyright © 2021 App Brewery. All rights reserved.
-//
-
 import Foundation
 
 struct CityName: Decodable {
