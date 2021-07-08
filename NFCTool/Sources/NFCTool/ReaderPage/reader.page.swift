a2da57c5ad20dc99e2b47555ca502fd3d4e236d3
Pavlo Boiko <boiko.impul@gmail.com>
2021-07-08 23:56:19 +0300

    Commented

--- NFCTool/Sources/NFCTool/ReaderPage/reader.page.swift
+++ NFCTool/Sources/NFCTool/ReaderPage/reader.page.swift
@@ -6,15 +6,24 @@ class ReaderPageAdapter: SCDLatticePageAdapter {
   	// page adapter initialization
   	override func load(_ path: String) {
     	super.load(path)
-		nfcManager = NFCManager(alertMessage: "Hold your iPhone near an NFC tag.")    	
+    	// initialize the NFCManager
+		nfcManager = NFCManager(alertMessage: "Hold your iPhone near an NFC tag.")
+
+		// connect the button action to read func
         self.readButton.onClick{_ in self.read()}
+
+        // Below code is only for Android
 	    self.page!.onExit.append(SCDWidgetsExitEventHandler{_ in
 	    	self.nfcManager?.disableForegroundDispatch()
-        })        
+        })     
   	}
   	
+  	// read func
   	func read() {
+  		// read NFC Tag
         nfcManager?.readTag { message in
+
+        	// get url and text from NFCMessage
         	guard let message = message else { return }
         	if let uriRecord = message.uriRecord {
         		self.urlLabel.text = "URL: \(uriRecord.url.absoluteString)"
--- NFCTool/Sources/NFCTool/WriterPage/writer.page.swift
+++ NFCTool/Sources/NFCTool/WriterPage/writer.page.swift
@@ -7,23 +7,37 @@ class WriterPageAdapter: SCDLatticePageAdapter {
   	// page adapter initialization
   	override func load(_ path: String) {
     	super.load(path)
-    	nfcManager = NFCManager(alertMessage: "Hold your iPhone near an NFC tag.")
-    	self.writeButton.onClick{_ in self.write()}
-  		self.page!.onExit.append(SCDWidgetsExitEventHandler{_ in
+    	// initialize the NFCManager
+		nfcManager = NFCManager(alertMessage: "Hold your iPhone near an NFC tag.")
+
+		// connect the button action to write func
+        self.writeButton.onClick{_ in self.write()}
+
+        // Below code is only for Android
+	    self.page!.onExit.append(SCDWidgetsExitEventHandler{_ in
 	    	self.nfcManager?.disableForegroundDispatch()
-        })   
+        })
   	}
   	
+    // write func
   	func write() {
+
+  		// prepare the NFCURIRecord from the URL
         var uriRecord: NFCURIRecord?
         if let url = URL(string: urlTextBox.text) {
             uriRecord = NFCURIRecord(url: url)
         }
         
+        // prepare the NFCTextRecord from the String and Locale
         let textRecord = NFCTextRecord(string: descriptionTextBox.text, locale: Locale(identifier: "en"))
+
+        // initialize the NFCMessage with NFCURIRecord and NFCTextRecord
         let nfcMessage = NFCMessage(uriRecord: uriRecord, textRecord: textRecord)
         
+        // initialize the NFCMessage
         nfcManager = NFCManager(alertMessage: "Hold your iPhone near an NFC tag.")
+
+        // write NFC Tag
         nfcManager?.writeTag(nfcMessage)
-    }  	
+    }	
 }
